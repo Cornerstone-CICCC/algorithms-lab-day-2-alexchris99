ad9a23152bc78891406106253971b6f86b0fd429
Christian Jimenez <cajt@outlook.com>
2024-10-01 10:44:53 -0800

    Excercise 5

--- ex05.js
+++ ex05.js
@@ -1,5 +1,22 @@
 // Exercise 5: Write a function `countConsonants` that takes a string
 // and returns the number of consonants in the string.
 // Example: countConsonants("hello world") should return 7.
+countConsonants = function(string){
+    let newString = string.split(" ")
+    newString = newString.join("")
+    newString = newString.toLowerCase()
+    newString = newString.split("")
+    
+    let consonats = ["q","w","r","t","y","p","s","d","f","g","h","j","k","l","z","x","c","v","b","n","m"]
+    let counter = 0
+    for(let i = 0; i < newString.length; i++){
+        for(let j = 0; j < consonats.length; j++){
+            if(newString[i] == consonats[j]){
+                counter += 1
+            }
+        }
+    }
+    return counter
+}
 
-console.log(countConsonants("hello world")); // Expected output: 7
+console.log(countConsonants("hello world Hi")); // Expected output: 7
