15efd37d8dd7be13f9ed3ff96bda4338949b2533
Christian Jimenez <cajt@outlook.com>
2024-10-01 10:18:31 -0800

    Excersice 4

--- ex04.js
+++ ex04.js
@@ -2,4 +2,19 @@
 // that appears the most times. If there is a tie, return the first character that reaches the maximum frequency.
 // Example: mostFrequentChar("javascript") should return "a".
 
-console.log(mostFrequentChar("javascript")); // Expected output: "a"
+mostFrequentChar = function(string){
+    let arr = string.split("")
+
+    let letters = arr.reduce((acumulator, index)=>{
+        if(!acumulator[index]){
+            acumulator[index] = 1
+        }else{
+            acumulator[index] +=1
+        }
+        return acumulator
+    }, {})
+
+    return Object.keys(letters).reduce((a, b) => letters[a] > letters[b] ? a : b)
+}
+
+console.log(mostFrequentChar("Hellooo")); // Expected output: "a"
