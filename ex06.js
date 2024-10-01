c39e16db5c2718b0eb1802ca2cb95bd96cffcfb1
Christian Jimenez <cajt@outlook.com>
2024-10-01 11:13:53 -0800

    Excercise 6

--- ex06.js
+++ ex06.js
@@ -3,4 +3,16 @@
 // Example: getQueryParams("https://example.com?search=test&page=2")
 // should return { search: "test", page: "2" }.
 
+getQueryParams = function(url){
+    let newUrl = url.split("?")
+    newUrl = newUrl[1]
+    newUrl = newUrl.split(/=|&/)
+ 
+    let obj = {}
+    obj[newUrl[0]] = newUrl[1]
+    obj[newUrl[2]] = newUrl[3]
+
+    return obj
+}
+
 console.log(getQueryParams("https://example.com?search=test&page=2")); // Expected output: { search: "test", page: "2" }
