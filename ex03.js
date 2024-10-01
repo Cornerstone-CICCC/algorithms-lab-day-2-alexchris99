854af08a16ed6089c09f32956604098aacd85eef
Christian Jimenez <cajt@outlook.com>
2024-10-01 09:39:10 -0800

    Excersice 3

--- ex03.js
+++ ex03.js
@@ -2,4 +2,14 @@
 // with one number missing and returns the missing number.
 // Example: findMissingNumber([1, 2, 4, 5]) should return 3.
 
-console.log(findMissingNumber([1, 2, 4, 5])); // Expected output: 3
+findMissingNumber = function(arr){
+    let counter = 1
+    for(let i = 0; i < arr.length; i++){
+        if(counter != arr[i]){
+            return counter
+        }
+        counter += 1
+    }
+}
+
+console.log(findMissingNumber([1, 2, 3,  5])); // Expected output: 3
