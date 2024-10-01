a919b39f39b881580bc54b09ce7763fbed284f68
Christian Jimenez <cajt@outlook.com>
2024-10-01 13:18:53 -0800

    Excercise 14

--- ex14.js
+++ ex14.js
@@ -2,6 +2,20 @@
 // `addItem(item)` to add an item to the cart, and `getItems()` to retrieve the list of added items.
 // Example: const cart = createCart(); cart.addItem("apple"); cart.getItems(); // ["apple"]
 
+
+
+createCart = function(){
+    return{
+        items: [],
+        addItem : function(item){
+            this.items.push(item)
+        },
+        getItems : function(){
+            return this.items
+        }
+    }
+}
+
 const cart = createCart();
 cart.addItem("apple");
 cart.addItem("banana");
