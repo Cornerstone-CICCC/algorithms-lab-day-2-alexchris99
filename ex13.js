f4208a2415b930deb4474b372ffa1a79d7890583
Christian Jimenez <cajt@outlook.com>
2024-10-01 12:55:27 -0800

    Excersice 13

--- ex13.js
+++ ex13.js
@@ -1,5 +1,12 @@
 // Exercise 13: Write a recursive function `sumRange` that takes a number `n`
 // and returns the sum of all numbers from 1 to `n`.
 // Example: sumRange(5) should return 15 (1 + 2 + 3 + 4 + 5).
+const sumRange = function(num){
+    if(num != 1){
+        return (num + sumRange(num-1))
+    }else{
+        return num
+    }
+}
 
 console.log(sumRange(5)); // Expected output: 15
